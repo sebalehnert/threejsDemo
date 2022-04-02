5e7f81059395e7d952a4f270d688024ea3faad90
seba <e>
2022-04-02 22:22:06 +0200

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,14 @@ const path = require('path');
 module.exports = {
   entry: './main.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, '../build'),
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../build'),
+    },
+    compress: false,
+    port: 8000,
+  }
 };
