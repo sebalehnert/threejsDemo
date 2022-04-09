0abaf82bd81c74ba935cdfef6bedda9cd651ecb8
seba <e>
2022-04-09 13:48:39 +0200

    cleanup files

--- main.js
+++ main.js
@@ -20,91 +20,13 @@ function init() {
 
 	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
 
-	// const defaultLight = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-	// defaultLight.position.set(0.5, 1, 0.25);
-	// scene.add(defaultLight);
-
-	//
-
 	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	// renderer.outputEncoding = THREE.sRGBEncoding;
-	// renderer.physicallyCorrectLights = true;
 	renderer.xr.enabled = true;
 	container.appendChild(renderer.domElement);
 
-	// Don't add the XREstimatedLight to the scene initially.
-	// It doesn't have any estimated lighting values until an AR session starts.
-
-	// const xrLight = new XREstimatedLight(renderer);
-
-	// xrLight.addEventListener('estimationstart', () => {
-
-	// 	// Swap the default light out for the estimated one one we start getting some estimated values.
-	// 	scene.add(xrLight);
-	// 	scene.remove(defaultLight);
-
-	// 	// The estimated lighting also provides an environment cubemap, which we can apply here.
-	// 	if (xrLight.environment) {
-
-	// 		scene.environment = xrLight.environment;
-
-	// 	}
-
-	// });
-
-	// xrLight.addEventListener('estimationend', () => {
-
-	// 	// Swap the lights back when we stop receiving estimated values.
-	// 	scene.add(defaultLight);
-	// 	scene.remove(xrLight);
-
-	// 	// Revert back to the default environment.
-	// 	scene.environment = defaultEnvironment;
-
-	// });
-
-	// //
-
-	// new RGBELoader()
-	// 	.setPath('textures/equirectangular/')
-	// 	.load('royal_esplanade_1k.hdr', function (texture) {
-
-	// 		texture.mapping = THREE.EquirectangularReflectionMapping;
-
-	// 		defaultEnvironment = texture;
-
-	// 		scene.environment = defaultEnvironment;
-
-	// 	});
-
-	//
-
-	// In order for lighting estimation to work, 'light-estimation' must be included as either an optional or required feature.
 	document.body.appendChild(ARButton.createButton(renderer));
-
-	//------------------
-	// var materials = [
-	// 	new THREE.MeshLambertMaterial({
-	// 		map: new THREE.TextureLoader().load('data/DSC_2222-cropped.jpg')
-	// 	}),
-	// 	new THREE.MeshLambertMaterial({
-	// 		map: new THREE.TextureLoader().load('data/DSC_2222-cropped.jpg')
-	// 	}),
-	// 	new THREE.MeshLambertMaterial({
-	// 		map: new THREE.TextureLoader().load('data/DSC_2222-cropped.jpg')
-	// 	}),
-	// 	new THREE.MeshLambertMaterial({
-	// 		map: new THREE.TextureLoader().load('data/DSC_2222-cropped.jpg')
-	// 	}),
-	// 	new THREE.MeshLambertMaterial({
-	// 		map: new THREE.TextureLoader().load('data/DSC_2222-cropped.jpg')
-	// 	}),
-	// 	new THREE.MeshLambertMaterial({
-	// 		map: new THREE.TextureLoader().load('data/DSC_2222-cropped.jpg')
-	// 	})
-	// ];
 
 	const materials = [
 			new THREE.MeshBasicMaterial({
@@ -137,17 +59,13 @@ function init() {
 	scene.add(boxGroup);
 
 	function onSelect() {
-
 	// 	ballGroup.position.set(0, 0, - 2).applyMatrix4(controller.matrixWorld);
 	// 	ballGroup.quaternion.setFromRotationMatrix(controller.matrixWorld);
-
 	}
 
 	controller = renderer.xr.getController(0);
 	controller.addEventListener('select', onSelect);
 	scene.add(controller);
-
-	//
 
 	window.addEventListener('resize', onWindowResize);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production'
   entry: './main.js',
   output: {
     filename: '[name].js',
