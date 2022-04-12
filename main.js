efd4b08a7de05ef5f1b69fc0fa347e23f67d5d5c
seba <e>
2022-04-12 22:42:24 +0200

    working instanced mesh

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+// import { TextBufferGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 let camera, controls, scene, renderer;
@@ -23,14 +24,13 @@ function init() {
 	scene = new THREE.Scene();
 	allGroup = new THREE.Group();
 	boxGroup = new THREE.Group();
-	allGroup.add(boxGroup);
+	// allGroup.add(boxGroup);
 	textGroup = new THREE.Group();
 	allGroup.add(textGroup);
 	allGroup.position.z = - 2;
 	scene.add(allGroup);
 
 	loadFont();
-	// createText();
 
 	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
 
@@ -39,14 +39,14 @@ function init() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	// renderer.xr.enabled = true;
 	container.appendChild(renderer.domElement);
-	controls = new OrbitControls( camera, container );
-	controls.listenToKeyEvents( window );
+	controls = new OrbitControls(camera, container);
+	controls.listenToKeyEvents(window);
 	controls.enabled = true;
 	// controls.update();
 
 	//-----4
-	controls.addEventListener('mousedown',myOnMouseDownFunction ,false);
-	function myOnMouseDownFunction( evt ) {
+	controls.addEventListener('mousedown', myOnMouseDownFunction, false);
+	function myOnMouseDownFunction(evt) {
 		// evt.preventDefault();
 		// var array = getMousePosition( contB1, evt.clientX, evt.clientY );
 		// onClickPosition.fromArray( array );
@@ -59,7 +59,7 @@ function init() {
 		// 	 controls.enabled = true;
 		// }
 		console.log("I am being called");
-	  }
+	}
 	//-----
 
 	// document.body.appendChild(ARButton.createButton(renderer));
@@ -91,8 +91,8 @@ function init() {
 	boxGroup.add(rotatingBox);
 
 	function onSelect() {
-		// 	ballGroup.position.set(0, 0, - 2).applyMatrix4(controller.matrixWorld);
-		// 	ballGroup.quaternion.setFromRotationMatrix(controller.matrixWorld);
+		// 	all.position.set(0, 0, - 2).applyMatrix4(controller.matrixWorld);
+		// 	allGroup.quaternion.setFromRotationMatrix(controller.matrixWorld);
 	}
 
 	// controller = renderer.xr.getController(0);
@@ -124,12 +124,16 @@ function render() {
 }
 
 function createText() {
-	console.log(font);
+	if (font === undefined) {
+		console.log("Font not loaded");
+		return;
+	}
+
 	let textGeo = new TextGeometry(text, {
 		font: font,
 
-		size: 1,
-		height: 0.3
+		size: 0.1,
+		height: 0.03
 		// curveSegments: curveSegments,
 
 		// bevelThickness: bevelThickness,
@@ -141,11 +145,11 @@ function createText() {
 	// const centerOffset = - 0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
 
 	let materials = [
-		new THREE.MeshBasicMaterial( { color: 0xff0000, flatShading: true } ), // front
-		new THREE.MeshBasicMaterial( { color: 0x444444 } ) // side
+		new THREE.MeshBasicMaterial({ color: 0xff0000 }), // front
+		new THREE.MeshBasicMaterial({ color: 0x444444 }) // side
 	];
 
-	let textMesh1 = new THREE.Mesh(textGeo, materials);
+	//let textMesh1 = new THREE.Mesh(textGeo, materials);
 
 	// textMesh1.position.x = centerOffset;
 	// textMesh1.position.y = hover;
@@ -154,7 +158,17 @@ function createText() {
 	// textMesh1.rotation.x = 0;
 	// textMesh1.rotation.y = Math.PI * 2;
 
-	textGroup.add(textMesh1);
+	let allTextMeshs = new THREE.InstancedMesh(textGeo, materials, 1);
+	allTextMeshs.instanceMatrix.setUsage(THREE.DynamicDrawUsage); // will be updated every frame
+	allTextMeshs.setMatrixAt(0, new THREE.Matrix4());
+
+	let boxGeom = new THREE.BoxGeometry(0.8, 0.8, 0.8);
+	let boxInstanced = new THREE.InstancedMesh(boxGeom, materials, 1);
+	boxInstanced.setMatrixAt(0, new THREE.Matrix4());
+
+	//textGroup.add(boxInstanced);
+	textGroup.add(allTextMeshs);
+	//textGroup.add(textMesh1);
 }
 
 function loadFont() {
@@ -162,9 +176,6 @@ function loadFont() {
 	const loader = new FontLoader();
 	loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (response) {
 		font = response;
-		console.log("loaded");
 		createText();
 	});
-	console.log("myfont");
-	console.log(font);
 }
